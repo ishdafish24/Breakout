69af710e44f956ee087fa7f217824ea0dc6e9ac2
ishdafish24 <51826348+ishdafish24@users.noreply.github.com>
2022-04-07 11:53:02 +0100

    Init

---
+++ Breakout.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Breakout extends JFrame
+{
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+
+    public Breakout()
+    {
+        super("My Alsome Game!!!");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setResizable(false);
+        Toolkit t = getToolkit();
+        Dimension d = t.getScreenSize();
+        System.out.println(d.getHeight());
+        System.out.println(d.getWidth());
+        setLocation(0, 0);
+        setVisible(true);
+    }
+
+}
